64230e48988b6f0f992d714e92d669fe5ee9e2fe
RastaClarity <allue1995@gmail.com>
2026-06-08 20:52:42 +0200

    Update appConfig.js

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -39,8 +39,8 @@ const BRAND = {
   subtagline:"Reserva, juega y gana recompensas",
 };
 
-const APP_VERSION = "RASTACUTS_2_9_3K_DEDUPE_AVATAR_HELPERS";
-const APP_VERSION_SHORT = "2.9.3k";
+const APP_VERSION = "RASTACUTS_2_9_4B_POINTS_HISTORY_DB";
+const APP_VERSION_SHORT = "2.9.4b";
 const APP_BUILD_DATE = "2026-06-08";
 const APP_SAFE_MODE_KEY = "rastaCutsSafeMode";
 
